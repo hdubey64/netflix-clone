338a0ba5613235e3d0415829c756383c61654a52
hdubey64 <hdubey64@gmail.com>
2023-01-31 17:18:45 +0530

    Hero Section Ready

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,7 +1,28 @@
-import React from "react";
+import { Container } from "reactstrap";
+import EmailInput from "../atoms/EmailInput";
+import Input from "../atoms/EmailInput";
+import Header from "../Header";
+import "./style.css";
 
 const HeroSection = () => {
-   return <div>HeroSection</div>;
+   return (
+      <div className="heroSection">
+         <Header />
+         <Container>
+            <div className="heroContent">
+               <h1>Unlimited movies, TV shows and more.</h1>
+               <h5> Watch anywhere. Cancel anytime.</h5>
+               <p>
+                  Ready to watch? Enter your email to create or restart your
+                  membership.
+               </p>
+               <div className="heroInput">
+                  <EmailInput />
+               </div>
+            </div>
+         </Container>
+      </div>
+   );
 };
 
 export default HeroSection;
