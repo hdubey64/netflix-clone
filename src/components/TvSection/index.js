bd25f6d372d99184b9caa2ae097fe9eb2d5493f4
hdubey64 <hdubey64@gmail.com>
2023-02-01 17:00:59 +0530

    TvSection Content & Css

--- src/components/TvSection/index.js
+++ src/components/TvSection/index.js
@@ -1,20 +1,43 @@
-import { Container } from "@mui/material";
+import { Container, Row, Col } from "reactstrap";
+import SectionBanner from "../atoms/SectionBanner";
 import SectionTitle from "../atoms/SectionTitle";
+import ReactPlayer from "react-player";
+import "./style.css";
 
 const TvSection = () => {
    return (
       <div>
-         <SectionTitle>
-            <Container>
-               <h1>Enjoy on your TV.</h1>
-               <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae
-                  vero rerum, repellat tenetur quis dignissimos, atque soluta
-                  eos nisi sit repellendus, libero tempore quaerat ab fuga
-                  aliquid iste ipsa iure.
-               </p>
-            </Container>
-         </SectionTitle>
+         <Container>
+            <div className="tvContent">
+               <Row>
+                  <Col md={12} lg={6} className="m-auto">
+                     <SectionTitle>
+                        <h1>Enjoy on your TV.</h1>
+                        <p>
+                           Watch on smart TVs, PlayStation, Xbox, Chromecast,
+                           Apple TV, Blu-ray players and more..
+                        </p>
+                     </SectionTitle>
+                  </Col>
+                  <Col md={12} lg={6} className="m-auto">
+                     <SectionBanner>
+                        <img
+                           src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png"
+                           alt="Banner"
+                        />
+                        <video
+                           src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-in-0819.m4v"
+                           muted
+                           autoPlay={"autoplay"}
+                           loop
+                           preLoad="auto"
+                           playsInline
+                        />
+                     </SectionBanner>
+                  </Col>
+               </Row>
+            </div>
+         </Container>
       </div>
    );
 };
