08569862ca9141b9d06ccb8a5209d9acdc4572dd
hdubey64 <hdubey64@gmail.com>
2023-02-01 16:55:03 +0530

    Section Title Children

--- src/components/atoms/SectionTitle/index.js
+++ src/components/atoms/SectionTitle/index.js
@@ -1,7 +1,12 @@
 import { Box } from "@mui/material";
+import "./style.css";
 
 const SectionTitle = ({ children }) => {
-   return <Box className="section-title">{children}</Box>;
+   return (
+      <div className="sectionTitle">
+         <div className="titleContent">{children}</div>
+      </div>
+   );
 };
 
 export default SectionTitle;
