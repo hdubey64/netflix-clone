b4dce0f6e18fe731dd91b7688391a3bdacf374be
hdubey64 <hdubey64@gmail.com>
2023-02-01 17:02:05 +0530

    <hr /> tag Removed

--- src/layouts/MainLayout/index.js
+++ src/layouts/MainLayout/index.js
@@ -1,12 +1,7 @@
 import "./style.css";
 
 const MainLayout = ({ children }) => {
-   return (
-      <>
-         <hr />
-         <div className="main-layout sectionBlack">{children}</div>
-      </>
-   );
+   return <div className="main-layout sectionBlack">{children}</div>;
 };
 
 export default MainLayout;
